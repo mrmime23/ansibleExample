a55915b9e30b09e7d78d84e3799d9f57f29c66a2
mrmime23 <143339797+mrmime23@users.noreply.github.com>
2023-08-30 18:48:12 +0200

    Update fitler.py

--- fitler.py
+++ fitler.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
-class FilterModule(object):
-    def filters(self):
+class filter_module(object):
+    def SomeFilters(self):
         return {
             'a_filter': self.a_filter
         }
 
     def a_filter(self, a_variable):
-        a_new_variable = a_variable + ' CRAZY NEW FILTER'
-        return a_new_variable
+        a_newvariable = a_variable + ' CRAZY NEW FILTER'
+        return a_newvariable
